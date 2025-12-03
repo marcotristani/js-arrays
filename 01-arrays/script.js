05de456772feb200301ec1dd8a7f02fbe29119ba
marcotristani <marco.tristani@hotmail.it>
2025-12-03 17:04:39 +0100

    flat:sistemato refuse e crata pagina iniziale

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -9,37 +9,44 @@ const teachers = [
 ]; // NON MODIFICARE QUESTA VARIABILE
 console.log("L'array originale è :", teachers);
 
+console.log("");
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
 console.log( "il quarto insegnante nell'array è :", fourthTeacher );
 
-
+console.log("");
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers.splice(4, 1, 'Patrick');
 console.log("L'array modificato è :", teachers, "....(Sostituito il quinto insegnante nell'array con 'Patrick)");
 
+console.log("");
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
 console.log("L'ultimo elemento rimosso dall'array è;", lastTeacher, "....L'array modificato è:", teachers);
 
+console.log("");
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
 console.log("Il primo elemento rimosso dall'array è;", firstTeacher, "....L'array modificato è:", teachers);
 
+console.log("");
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa');
 console.log("L'arrey modificato è :", teachers, "....(Aggiunto un insegnante di nome Vanessa alla fine dell' array)");
 
+console.log("");
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 teachers.unshift('Sarah');
 console.log("L'arrey modificato è :", teachers, "....(Aggiunto un insegnante di nome Sarah all'inizio dell' array)");
 
+console.log("");
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 const lewisIndex = teachers.indexOf('Lewis');
 console.log("L'indice corrispondente all'insegnante Lewis è:", lewisIndex);
 
+console.log("");
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 let isTeachersEmpty = null;
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,70 +1,94 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 console.log("L'array originale è :", teachers);
+
+console.log("");
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.toReversed();;
-console.log("1.L'array capovolto con la funzione toReversed:", reversedTeachers);
+const reversedTeachers = teachers.toReversed();
+console.log(
+  "1.L'array capovolto con la funzione toReversed:",
+  reversedTeachers
+);
 
 //OPPURE CON CICLO FOR(push)
 const reversedTeachersForPush = [];
-for(let i = teachers.length - 1; i>= 0; i--){
+for (let i = teachers.length - 1; i >= 0; i--) {
   reversedTeachersForPush.push(teachers[i]);
 }
-console.log("1.L'array capovolto con ciclo for e funzione push:", reversedTeachersForPush);
+console.log(
+  "1.L'array capovolto con ciclo for e funzione push:",
+  reversedTeachersForPush
+);
 
 //OPPURE CON CICLO FOR(unshift)
 const reversedTeachersForUnShift = [];
-for(let i = 0; i < teachers.length; i++ ){
+for (let i = 0; i < teachers.length; i++) {
   reversedTeachersForUnShift.unshift(teachers[i]);
 }
-console.log("1.L'array capovolto con ciclo for e funzione unshift:", reversedTeachersForUnShift);
+console.log(
+  "1.L'array capovolto con ciclo for e funzione unshift:",
+  reversedTeachersForUnShift
+);
 
-
+console.log("");
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
-for(let i = 0; i< teachers.length ; i++){
-  if(teachers[i].length >= 5){
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
     longNames.push(teachers[i]);
   }
 }
-console.log("2.L'array longNames contenente i nomi degli insegnanti lunghi almeno 5 caratteri è:", longNames);
+console.log(
+  "2.L'array longNames contenente i nomi degli insegnanti lunghi almeno 5 caratteri è:",
+  longNames
+);
 
+console.log("");
 // 3. Rimuovi 'Ed' dall'array teachers
-const indexEd = teachers.indexOf('Ed');
+const indexEd = teachers.indexOf("Ed");
 teachers.splice(indexEd, 1);
 console.log("3.Rimosso il nome Ed dall'array:", teachers);
 
+console.log("");
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = teachers.includes('Fabio');
-console.log("4.E' presente Fabio nell'array?:", isFabioPresent, "....(eseguito con la funzione includes)");
- 
+const isFabioPresent = teachers.includes("Fabio");
+console.log(
+  "4.E' presente Fabio nell'array?:",
+  isFabioPresent,
+  "....(eseguito con la funzione includes)"
+);
+
 //4. ESEGUITO CON CONDIZIONI
-const indexFabio = teachers.indexOf('Fabio');
+const indexFabio = teachers.indexOf("Fabio");
 let isFabioPresentIf = true;
-if(indexFabio === -1){
+if (indexFabio === -1) {
   isFabioPresentIf = false;
 }
-console.log("4.E' presente Fabio nell'array?:", isFabioPresentIf, "....(eseguito con la funzione indexOf e condizione di esistenza)");
+console.log(
+  "4.E' presente Fabio nell'array?:",
+  isFabioPresentIf,
+  "....(eseguito con la funzione indexOf e condizione di esistenza)"
+);
 
+console.log("");
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.toString();
-console.log("5.Stringa degli elemnti dell'array ottenuta con funzione toString:", teachersString);
+console.log(
+  "5.Stringa degli elemnti dell'array ottenuta con funzione toString:",
+  teachersString
+);
 
-let teachersStringFor = '';
-for(i=0; i< teachers.length; i++){
+//ESEGUITO CON FOR
+let teachersStringFor = "";
+for (i = 0; i < teachers.length; i++) {
   teachersStringFor += `${teachers[i]}`;
-  if( i !== teachers.length - 1 ){
+  if (i !== teachers.length - 1) {
     teachersStringFor += ",";
   }
 }
-console.log("5.Stringa degli elemnti dell'array ottenuta con ciclo For:", teachersStringFor);
+console.log(
+  "5.Stringa degli elemnti dell'array ottenuta con ciclo For:",
+  teachersStringFor
+);
